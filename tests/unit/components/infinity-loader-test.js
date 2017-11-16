f93244b901ab634d5198cc67ec9b1afce47e9e48
snewcomer24 <snewcomer24@gmail.com>
2017-11-16 12:37:19 -0800

    remove some infinity-loader integration tests

--- tests/unit/components/infinity-loader-test.js
+++ tests/unit/components/infinity-loader-test.js
@@ -19,35 +19,6 @@ test('it renders', function(assert) {
   assert.equal(component._state, 'inDOM');
 });
 
-test('it will not destroy on load unless set', function(assert) {
-  assert.expect(3);
-
-  var infinityModelStub = [
-    {id: 1, name: 'Tomato'},
-    {id: 2, name: 'Potato'}
-  ];
-
-  var component = this.subject({ infinityModel: infinityModelStub });
-  this.render();
-
-  assert.equal(component.get('destroyOnInfinity'), false);
-
-  Ember.run(function() {
-    component.set('infinityModel.reachedInfinity', true);
-  });
-
-  assert.equal(component._state, 'inDOM');
-
-  Ember.run(function() {
-    component.set('destroyOnInfinity', true);
-  });
-
-  // In Ember 2.8, there was an optimization that meant tearing
-  // down views would return them to the preRender state, ready
-  // to be reinserted. See here: https://github.com/emberjs/ember.js/pull/13648#issuecomment-225334352
-  assert.notEqual(component._state, 'inDOM');
-});
-
 test('it changes text property', function(assert) {
   assert.expect(2);
 
@@ -69,99 +40,6 @@ test('it changes text property', function(assert) {
 
   componentText = $.trim(component.$().text());
   assert.equal(componentText, "Infinite Model Entirely Loaded.");
-});
-
-test('it uses the window as the scrollable element', function(assert) {
-  assert.expect(1);
-  var component = this.subject();
-  this.render();
-  var scrollable = component.get("_scrollable");
-  assert.equal(scrollable[0], window);
-});
-
-test('it uses the provided scrollable element', function(assert) {
-  assert.expect(1);
-  $(document.body).append("<div id='content'/>");
-  var component = this.subject({scrollable: "#content"});
-  this.render();
-  var scrollable = component.get("_scrollable");
-  assert.equal(scrollable[0], $("#content")[0]);
-});
-
-test('it throws error when scrollable element is not found', function(assert) {
-  assert.expect(1);
-
-  const component = this.subject({scrollable: "#nonexistent"});
-  try {
-    component.didInsertElement();
-  } catch(e) {
-    return assert.ok(e.message.indexOf('Ember Infinity: No scrollable element found for') > -1);
-  }
-});
-
-test('it throws error when multiple scrollable elements are found', function(assert) {
-  assert.expect(1);
-  $(document.body).append("<div class='hello'><div/>");
-  $(document.body).append("<div class='hello'><div/>");
-
-  const component = this.subject({scrollable: ".hello"});
-  try {
-    component.didInsertElement();
-  } catch(e) {
-    return assert.ok(e.message.indexOf('Ember Infinity: Multiple scrollable elements found for') > -1);
-  }
-});
-
-test('it throws error when scrollable is something other than nothing or string', function(assert) {
-  assert.expect(1);
-  $(document.body).append("<div id='content'/>");
-  const component = this.subject({scrollable: $("#content")});
-
-  try {
-    component.didInsertElement();
-  } catch(e) {
-    return assert.ok(e.message.indexOf('Ember Infinity: Scrollable must either be a css selector string or left empty to default to window') > -1);
-  }
-});
-
-test('it checks if in view on the scroll event', function(assert) {
-  assert.expect(1);
-  var done = assert.async();
-
-  var component = this.subject();
-
-  var isAfterRender = false;
-  component.set('_loadMoreIfNeeded', function() {
-    if (isAfterRender) {
-      assert.ok(true);
-      done();
-    }
-  });
-
-  this.render();
-
-  isAfterRender = true;
-  $(window).trigger('scroll');
-});
-
-test('it checks if in view on the resize event', function(assert) {
-  assert.expect(1);
-  var done = assert.async();
-
-  var component = this.subject();
-
-  var isAfterRender = false;
-  component.set('_loadMoreIfNeeded', function() {
-    if (isAfterRender) {
-      assert.ok(true);
-      done();
-    }
-  });
-
-  this.render();
-
-  isAfterRender = true;
-  $(window).trigger('resize');
 });
 
 test('it checks if in view after model is pushed', function(assert) {
